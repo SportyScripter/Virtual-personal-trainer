6b00fd1f4ecd51a3f314c22441681cc04eb6a395
SportyScripter <adrian.kacorzyk@edu.uekat.pl>
2025-07-30 20:14:18 +0200

    add endpoint to show list of all exercise and video created by actual active user

--- backend/schemas/exercise.py
+++ backend/schemas/exercise.py
@@ -1,4 +1,7 @@
 from pydantic import BaseModel
+from typing import List
+from datetime import datetime
+from schemas.exercise_video import UserExerciseVideoResponse
 
 
 class ExerciseCreate(BaseModel):
@@ -13,6 +16,17 @@ class ResponseModelExercise(BaseModel):
     exercise_name: str
     body_part_id: int
     description: str
+
+    class Config:
+        orm_mode = True
+
+
+class UserExerciseResponse(BaseModel):
+    id: int
+    exercise_name: str
+    description: str | None
+    body_part_name: str
+    videos: List[UserExerciseVideoResponse] = []
 
     class Config:
         orm_mode = True
--- backend/schemas/exercise_video.py
+++ backend/schemas/exercise_video.py
@@ -1,6 +1,17 @@
 from pydantic import BaseModel
+from datetime import datetime
+
 
 class CreateExerciseVideo(BaseModel):
     user_id: int
     exercicise_id: int
     video_path: str
+
+
+class UserExerciseVideoResponse(BaseModel):
+    id: int
+    video_path: str
+    create_at: datetime
+
+    class Config:
+        orm_mode = True
